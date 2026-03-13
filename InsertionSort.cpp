3f734cf2925164a7811475ba507ebc3ea8718fc3
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-13 14:15:23 +0700

    menyelesaikan void insertionsort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -38,3 +38,11 @@ void insertionsort(){ //procedure insertion sort
             arr[j + 1] = arr[j];
             j--;
         }
+        arr[j + 1] = temp;
+
+        cout << "\nStep " << i << ": ";
+        for (int k = 0; k < n; k++){
+            cout << arr[k] << " ";
+        }
+    }
+}
