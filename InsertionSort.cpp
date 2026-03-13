44d242af115c7613636bd6c8f896d69ab94c5787
coannanaufal-debug <coanna.naufal@gmail.com>
2026-03-13 14:17:00 +0700

    membuat void display menunjukan elemen array

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -46,3 +46,16 @@ void insertionsort(){ //procedure insertion sort
         }
     }
 }
+void display()
+{
+    cout << endl;
+    cout << "\n================================";
+    cout << "\n==========Element Array=========" << endl;
+    cout << "\n================================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
